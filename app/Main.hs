ff23587273a824cbae7f16d0e31c0bb95413a180
fumyk <prchnq@gmail.com>
2022-04-09 23:47:54 +0700

    update package.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Translator
+import Text.Parsec.ByteString (Parser)
+import Text.Parsec (string, parseTest)
+import qualified Data.ByteString.Char8 as C
 
 main :: IO ()
 main = print $ translateAbstract exampleTmpModule
+
+type Kw = String 
+
+tmpTestParser :: Parser Kw
+tmpTestParser = string "yo"
+
+main2 :: IO ()
+main2 =
+    parseTest tmpTestParser (C.pack "yo qwe")
